4bfb0761cc430343e50264a345bce28b8003fed0
Robin <1933549736@qq.com>
2016-02-17 12:20:07 +0800

    Add timer type list
    justify the timer types

---
+++ Week-01/MagicTimer/MagicTimer/MagicTimerLabel.swift
@@ -0,0 +1,13 @@
+//
+//  MagicTimerLabel.swift
+//  MagicTimer
+//
+//  Created by Robin on 2/17/16.
+//  Copyright © 2016 Robin. All rights reserved.
+//
+
+import UIKit
+
+public class MagicTimerLabel: UILabel {
+    
+}
--- Week-01/MagicTimer/MagicTimer/MasterViewController.swift
+++ Week-01/MagicTimer/MagicTimer/MasterViewController.swift
@@ -9,18 +9,14 @@
 import UIKit
 
 class MasterViewController: UITableViewController {
-
+    
     var detailViewController: DetailViewController? = nil
-    var objects = [AnyObject]()
 
+    let titles = ["Using existing UILabel, stopwatch", "Self as label, free styling just like usual", "Countdown Timer", "Stopwatch with controls and time format", "Countdown timer with controls", "Countdown finish callback with Delegate", "Countdown finish callback with block", "Change color when time > 10 sec", "Custom Text return from delegate", "Modify Stopwatch time", "Modify Timer time", "Count beyond 23 hour limit of HH", "Countdown timer in text"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.navigationItem.leftBarButtonItem = self.editButtonItem()
-
-        let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
-        self.navigationItem.rightBarButtonItem = addButton
         if let split = self.splitViewController {
             let controllers = split.viewControllers
             self.detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
@@ -36,19 +32,14 @@ class MasterViewController: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    func insertNewObject(sender: AnyObject) {
-        objects.insert(NSDate(), atIndex: 0)
-        let indexPath = NSIndexPath(forRow: 0, inSection: 0)
-        self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
-    }
+ 
 
     // MARK: - Segues
 
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "showDetail" {
             if let indexPath = self.tableView.indexPathForSelectedRow {
-                let object = objects[indexPath.row] as! NSDate
+                let object = titles[indexPath.row] 
                 let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
                 controller.detailItem = object
                 controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
@@ -64,29 +55,15 @@ class MasterViewController: UITableViewController {
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return objects.count
+        return  titles.count
     }
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
-
-        let object = objects[indexPath.row] as! NSDate
-        cell.textLabel!.text = object.description
+ 
+        cell.textLabel?.text = "\(indexPath.row). " + titles[indexPath.row]
+        
         return cell
-    }
-
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            objects.removeAtIndex(indexPath.row)
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
-        }
     }
 
 
